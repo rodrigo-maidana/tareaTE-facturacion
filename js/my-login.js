2fd89715c890cb5409def99b8919f1d3019fe76a
Rodrigo Maidana <114775631+romaidana29@users.noreply.github.com>
2023-09-26 17:45:06 -0400

    Mejoras en register.html

--- js/my-login.js
+++ js/my-login.js
@@ -55,3 +55,67 @@ function verifyAuth() {
     window.location.href = "/index.html";
   }
 }
+
+function verifyRegister() {
+  const name = document.getElementById("name");
+  const username = document.getElementById("username");
+  const email = document.getElementById("email");
+  const password = document.getElementById("password");
+  const agree = document.getElementById("agree");
+
+  // Valida el campo `name`.
+  if (name.value === "") {
+    // El campo `name` está vacío.
+    name.classList.add("is-invalid");
+    name.nextElementSibling.textContent = "El nombre es obligatorio.";
+    return false;
+  } else {
+    name.classList.add("is-valid");
+  }
+
+  // Valida el campo `username`.
+  if (username.value === "") {
+    // El campo `username` está vacío.
+    username.classList.add("is-invalid");
+    username.nextElementSibling.textContent =
+      "El nombre de usuario es obligatorio.";
+    return false;
+  } else {
+    username.classList.add("is-valid");
+  }
+
+  // Valida el campo `email`.
+  if (!email.value.includes("@") || !email.value.includes(".")) {
+    // El correo electrónico no es válido.
+    email.classList.add("is-invalid");
+    email.nextElementSibling.textContent =
+      "El correo electrónico no es válido.";
+    return false;
+  } else {
+    email.classList.add("is-valid");
+  }
+
+  // Valida el campo `password`.
+  if (password.value === "") {
+    // El campo `password` está vacío.
+    password.classList.add("is-invalid");
+    password.nextElementSibling.textContent = "La contraseña es obligatoria.";
+    return false;
+  } else {
+    password.classList.add("is-valid");
+  }
+
+  // Valida el campo `agree`.
+  if (!agree.checked) {
+    // El campo `agree` no está marcado.
+    agree.classList.add("is-invalid");
+    agree.nextElementSibling.nextElementSibling.textContent =
+      "Debes aceptar los términos y condiciones.";
+    return false;
+  } else {
+    agree.classList.add("is-valid");
+  }
+
+  // Todos los campos son válidos.
+  return true;
+}
